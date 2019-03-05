684c4dc4fdedccaa0fd9d83a390a89c5f60d03d6
Praveen <leavemeatext@gmail.com>
2019-03-05 18:22:54 +0530

    added exact routes

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs')
+const path = require('path');
 const metadata = require('./metadata/metadata')
-// create express app
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/getPages',(req,res)=>{
-    res.json('{no_of_pages:2}')
+app.get('/getPages/getInsuredPage',(req,res)=>{
+    fs.readFile(path.join(__dirname,'Insured_Response.json'),'utf-8',(err,content)=>{
+        res.json(JSON.parse(content));
+     })
 })
-app.get('/getPages/:reqId', (req, res) => {
-    metadata.getData(req,res)
+app.get('/getPages/getAddnInsured', (req, res) => {
+    fs.readFile(path.join(__dirname,'AdditionalInsured_Response.json'),'utf-8',(err,content)=>{
+        res.json(JSON.parse(content));
+     })
 });
+
+app.get('/getPages',(req,res)=>{
+    fs.readFile(path.join(__dirname,'getPages.json'),'utf-8',(err,content)=>{
+        res.json(JSON.parse(content));
+     })
+})
 
 
 
--- metadata/metadata.js
+++ metadata/metadata.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const path = require('path');
 exports.getData = (req,res)=>{
 
-    const id = req.params.reqId;
+    const id = parseInt(req.params.reqId);
 
-    if(id==1){
+    if(id===1){
         fs.readFile(path.join(__dirname,'Insured_Response.json'),'utf-8',(err,content)=>{
             res.json(JSON.parse(content));
          })
-    }  if(id==2){
+    }  if(id===2){
         fs.readFile(path.join(__dirname,'AdditionalInsured_Response.json'),'utf-8',(err,content)=>{
             res.json(JSON.parse(content));
          })
