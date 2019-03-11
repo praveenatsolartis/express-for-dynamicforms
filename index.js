52661cea09b36135c6bf016dff7cbe0ea4317eed
Praveen <leavemeatext@gmail.com>
2019-03-11 14:36:37 +0530

    added 2 more pages

--- index.js
+++ index.js
@@ -4,16 +4,40 @@ const fs = require('fs')
 const path = require('path');
 const metadata = require('./metadata/metadata')
 const app = express();
+
+
+const allowCrossDomain = (req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(allowCrossDomain);
 
 app.get('/getPages/getInsuredPage',(req,res)=>{
     fs.readFile(path.join(__dirname,'Insured_Response.json'),'utf-8',(err,content)=>{
         res.json(JSON.parse(content));
      })
 })
+app.get('/getPages/getAddnInformation', (req, res) => {
+    fs.readFile(path.join(__dirname,'AdditionalInformation.json'),'utf-8',(err,content)=>{
+        res.json(JSON.parse(content));
+     })
+});
+
 app.get('/getPages/getAddnInsured', (req, res) => {
-    fs.readFile(path.join(__dirname,'AdditionalInsured_Response.json'),'utf-8',(err,content)=>{
+    fs.readFile(path.join(__dirname,'AdditionalInsured.json'),'utf-8',(err,content)=>{
+        res.json(JSON.parse(content));
+     })
+});
+
+app.get('/getPages/getAddnQuestions', (req, res) => {
+    fs.readFile(path.join(__dirname,'ApplicationQuestions.json'),'utf-8',(err,content)=>{
         res.json(JSON.parse(content));
      })
 });
